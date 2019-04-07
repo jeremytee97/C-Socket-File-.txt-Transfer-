ffa608eef83e3e950b401fffdc89ab6e0aa0540a
Jeremy Tee <teej1@student.unimelb.edu.au>
2019-04-07 16:48:27 +1000

    Initial commit of the files

---
+++ client.c
@@ -0,0 +1,146 @@
+/* A simple client program for server.c
+
+   To compile: gcc client.c -o client
+
+   To run: start the server, then the client */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <strings.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <unistd.h>
+
+#define LENGTH 256
+ssize_t total = 0;
+void sendfile(FILE *fp, int sockfd);
+int main(int argc, char ** argv)
+{
+    int sockfd, portno, n;
+    struct sockaddr_in serv_addr;
+    struct hostent * server;
+    const char close_conn[] = "GOODBYE-CLOSE-TCP";
+
+
+    if (argc < 3)
+    {
+        fprintf(stderr, "usage %s hostname port\n", argv[0]);
+        exit(0);
+    }
+
+    portno = atoi(argv[2]);
+
+    /* Translate host name into peer's IP address ;
+     * This is name translation service by the operating system
+     */
+    server = gethostbyname(argv[1]);
+
+    if (server == NULL)
+    {
+        fprintf(stderr, "ERROR, no such host\n");
+        exit(0);
+    }
+
+    /* Building data structures for socket */
+
+    bzero((char *)&serv_addr, sizeof(serv_addr));
+
+    serv_addr.sin_family = AF_INET;
+
+    bcopy(server->h_addr_list[0], (char *)&serv_addr.sin_addr.s_addr, server->h_length);
+
+    serv_addr.sin_port = htons(portno);
+
+    /* Create TCP socket -- active open
+    * Preliminary steps: Setup: creation of active open socket
+    */
+
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (sockfd < 0)
+    {
+        perror("ERROR opening socket");
+        exit(0);
+    }
+
+    if (connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+    {
+        perror("ERROR connecting");
+        exit(0);
+    }
+
+    /* Do processing
+    */
+    //while(1){
+        /* printf("Please enter the message: ");
+
+        bzero(buffer, LENGTH);
+
+        fgets(buffer, 255, stdin);
+        */
+
+   ssize_t total=0;
+   FILE* fp = fopen(argv[3], "rb");
+
+   if (fp == NULL){
+        perror("Cannot open file");
+        exit(1);
+   }
+
+   sendfile(fp, sockfd);
+ //puts("Send Success");
+   printf("Send Success, NumBytes = %ld\n", total);
+   fclose(fp);
+   close(sockfd);
+        // if(strncmp(buffer, close_conn, 17) == 0){
+        //     close(sockfd);
+        //     return 0;
+        // }
+        //
+        // if (n < 0)
+        // {
+        //     perror("ERROR writing to socket");
+        //     exit(0);
+        // }
+
+        // bzero(buffer, LENGTH);
+        //
+        // n = read(sockfd, buffer, 255);
+        //
+        // if (n < 0)
+        // {
+        //     perror("ERROR reading from socket");
+        //     exit(0);
+        // }
+        //
+        // printf("%s\n", buffer);
+
+    //}
+    return 0;
+}
+
+
+void sendfile(FILE *fp, int sockfd)
+{
+    int n;
+    char buffer[LENGTH];
+    while ((n = fread(buffer, sizeof(char), LENGTH, fp)) > 0)
+    {
+	    total+=n;
+        if (n != LENGTH && ferror(fp))
+        {
+            perror("Read File Error");
+            exit(1);
+        }
+
+        if (send(sockfd, buffer, n, 0) < 0)
+        {
+            perror("Can't send file");
+            exit(1);
+        }
+        memset(buffer, 0, LENGTH);
+    }
+}
---
+++ server.c
@@ -0,0 +1,161 @@
+/* A simple server in the internet domain using TCP
+The port number is passed as an argument
+
+
+ To compile: gcc server.c -o server
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <strings.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <ctype.h>
+
+#define LENGTH 256
+ssize_t total = 0;
+void writefile(FILE *fp, int sockfd);
+int main(int argc, char **argv)
+{
+	int sockfd, newsockfd, portno;// clilen;
+	char buffer[256];
+	struct sockaddr_in serv_addr, cli_addr;
+	socklen_t clilen;
+	const char close_conn[] = "GOODBYE-CLOSE-TCP";
+	int n;
+
+	if (argc < 2)
+	{
+		fprintf(stderr,"ERROR, no port provided\n");
+		exit(1);
+	}
+
+	 /* Create TCP socket */
+
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+	if (sockfd < 0)
+	{
+		perror("ERROR opening socket");
+		exit(1);
+	}
+
+
+	bzero((char *) &serv_addr, sizeof(serv_addr));
+
+	portno = atoi(argv[1]);
+
+	/* Create address we're going to listen on (given port number)
+	 - converted to network byte order & any IP address for
+	 this machine */
+
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_addr.s_addr = INADDR_ANY;
+	serv_addr.sin_port = htons(portno);  // store in machine-neutral format
+
+	 /* Bind address to the socket */
+
+	if (bind(sockfd, (struct sockaddr *) &serv_addr,
+			sizeof(serv_addr)) < 0)
+	{
+		perror("ERROR on binding");
+		exit(1);
+	}
+
+	/* Listen on socket - means we're ready to accept connections -
+	 incoming connection requests will be queued */
+
+	listen(sockfd,5);
+
+	clilen = sizeof(cli_addr);
+
+	/* Accept a connection - block until a connection is ready to
+	 be accepted. Get back a new file descriptor to communicate on. */
+	newsockfd = accept(	sockfd, (struct sockaddr *) &cli_addr,
+						&clilen);
+
+	if (newsockfd < 0)
+	{
+		perror("ERROR on accept");
+		exit(1);
+	}
+	bzero(buffer,256);
+
+		/* Read characters from the connection,
+			then process */
+	//while(1)
+		// n = read(newsockfd,buffer,255);
+		//
+		// if (n < 0)
+		// {
+		// 	perror("ERROR reading from socket");
+		// 	exit(1);
+		// }
+		//
+		// printf("Here is the message: %s\n",buffer);
+		//
+		// if(strncmp(buffer, close_conn, 17) == 0){
+		// 	close(sockfd);
+		// 	return 0;
+        // }
+		//for Q2 purpose
+		// for (int i = 0; i < strlen(buffer); i++){
+		// 	buffer[i] = toupper(buffer[i]);
+		// }
+		//
+		// n = write(newsockfd,buffer,255);
+		//
+		// if (n < 0)
+		// {
+		// 	perror("ERROR writing to socket");
+		// 	exit(1);
+		// }
+
+	FILE *fp = fopen("test1.txt", "wb");
+    if (fp == NULL)
+    {
+        perror("Cannot open file");
+        exit(1);
+    }
+
+    char addr[INET_ADDRSTRLEN];
+    printf("Start receive file");
+    writefile(fp, newsockfd);
+    printf("Receive Success, NumBytes = %ld\n", total);
+
+    fclose(fp);
+    close(newsockfd);
+	//}
+
+	/* close socket */
+
+	close(sockfd);
+
+	return 0;
+}
+
+
+void writefile(FILE *fp, int sockfd)
+{
+    ssize_t n;
+    char buff[LENGTH];
+    while ((n = recv(sockfd, buff, LENGTH, 0)) > 0)
+    {
+	    total+=n;
+        if (n == -1)
+        {
+            perror("Receive File Error");
+            exit(1);
+        }
+
+        if (fwrite(buff, sizeof(char), n, fp) != n)
+        {
+            perror("Write File Error");
+            exit(1);
+        }
+        memset(buff, 0, LENGTH);
+    }
+}
